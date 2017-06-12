9fd1459a0299ceba0ee408b2a4082597b96d60db
Pratapi Hemant Patel <hemantsonu20@gmail.com>
2017-06-12 11:49:48 +0530

    renamed package

--- gateway-service/src/main/java/com/github/swagger/gateway/config/SwaggerConfig.java
+++ gateway-service/src/main/java/com/github/swagger/gateway/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.github.swagger.product.config;
+package com.github.swagger.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
